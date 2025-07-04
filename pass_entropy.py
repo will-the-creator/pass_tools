dd4d78a3d69a5493220fcc73a11d6e086afee0ec
will-the-creator <hoopa05.cascade@icloud.com>
2025-07-03 21:40:37 -0600

    simplify logic in 'pass_ent' and 'pass_gen'

--- pass_entropy.py
+++ pass_entropy.py
@@ -1,38 +1,14 @@
 import math
-
-def calc_ent(password):
-
-    lower = "abcdefghijklmnopqrstuvwxyz"
-    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    nums = "1234567890"
-    special = "!@#$%^&*()_-+=[]{}\;:,.<>?/~`"
-
-    # determine char_set
-    char_size = 0
-    if any(c in lower for c in password):
-        char_size += len(lower)
-    if any(c in upper for c in password):
-        char_size += len(upper)
-    if any(c in nums for c in password):
-        char_size += len(nums)
-    if any(c in special for c in password):
-        char_size += len(special)
-
-    if char_size == 0:
-        return 0
-    entropy = len(password) * math.log2(char_size)
-    return entropy
-
-def main():
-    password = input("Never share your password with anyone\n\n enter test password: ")
-    entropy = calc_ent(password)
-    print(f"\nEntropy: {entropy:.2f} bits")
-
-    if entropy < 40:
-        print("\nWeak Password.")
-    elif entropy < 60:
-        print("\ncould be better, mid ahh password")
-    else:
-        print("\nStrong password")
-if __name__ == "__main__":
-    main()
+import string
+def calc_ent(pw):
+    sets = [
+        string.ascii_letters,
+        string.digits,
+        string.punctuation
+    ]
+    chars = sum(len(s) for s in sets if any(c in s for c in pw))
+    return 0 if chars == 0 else len(pw) * math.log2(chars)
+pw = input("enter password: ")
+e = calc_ent(pw)
+print(f" {e:.2f} bits")
+print("\nhell naw" if e < 40 else "\nmid ahh password" if e < 60 else "\nstrong")
--- pass_gen.py
+++ pass_gen.py
@@ -1,12 +1,2 @@
-import random
-import string 
-
-def gen_pass(length=16):
-    chars = string.ascii_letters + string.digits
-    password = ''.join(random.choice(chars) for _ in range(length))
-    return password
-
-if __name__ == "__main__":
-    
-    password_length = 16
-    print(f"Generated password: {gen_pass(password_length)}")
+import random, string
+print(''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=16)))
